2dd7d367007da9708fc24cd6bf6011b92b51ce7d
Kavya889 <Kavyakutty65@gmail.com>
2022-09-22 12:18:42 +0200

    Add login page

---
+++ login.js
@@ -0,0 +1,16 @@
+const loginForm = document.getElementById("login-form");
+const loginButton = document.getElementById("login-form-submit");
+const loginErrorMsg = document.getElementById("login-error-msg");
+
+loginButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    const username = loginForm.username.value;
+    const password = loginForm.password.value;
+
+    if (username === "Iamuser" && password === "student") {
+        alert("Great you entered");
+        location.reload();
+    } else {
+        loginErrorMsg.style.opacity = 1;
+    }
+})

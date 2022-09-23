fb8b5d7b3b14318e00e812826f133ea414b5fb94
Liao <liao.yiqiang@qq.com>
2022-09-23 12:56:44 +0200

    Add alerts

--- create.js
+++ create.js
@@ -6,14 +6,19 @@ loginButton.addEventListener("click", (e) => {
     
     
     const username_len=createForm.username_field.value.length
-    
+    const password=createForm.password_field.value
+    const confirm_password=createForm.confirm_password_field.value
     console.log(username_len);
 
-    if (username_len<=16 ) 
+    if(password!=confirm_password)
     {
-        alert("created account");
-        location.reload();
+        alert("Password doesn't match!");
+    }
+    else if (username_len>16) 
+    {
+        alert("The username cannot be longer than 16 letters.");
     } else{
-        alert("login failed");
+        alert("Created account successfully");
+        window.location.href="login.html"
     }
 })
